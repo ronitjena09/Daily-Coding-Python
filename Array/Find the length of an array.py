1052135234f8d7e51d99966097d78fe48cc3bcf7
Ronit Jena <ronitjena64@gmail.com>
2023-12-12 15:04:10 +0530

    regular commit

--- Array/Find the length of an array.py
+++ Array/Find the length of an array.py
@@ -1,13 +1,7 @@
-#For Given value: 
-#arr = [2,5,4,6,8,6,3,4,5,6,7,4,5,5,6]
-#print(len(arr))
+arr = []
+num = int(input())
 
+for i in range(num):
+    arr.append(i)
 
-#For getting a value:
-#arr = []
-#num = int(input())
-
-#for i in range(num):
-#    arr.append(i)
-    
-#print(len(arr))
+print(len(arr))
