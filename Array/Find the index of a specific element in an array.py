99187aa48b2a0605a5b0b694f262425231a90a45
Ronit Jena <ronitjena64@gmail.com>
2023-12-14 21:14:09 +0530

    regular commit

--- Array/Find the index of a specific element in an array.py
+++ Array/Find the index of a specific element in an array.py
@@ -1,1 +1,19 @@
+def indexEle(arr, element):
+    try:  # if element present in array
+        return arr.index(element)  # return the element index
+    except:
+        return -1
 
+
+num = int(input())  # size of array
+arr = []  # creating array
+
+
+for _ in range(num):  # defining elements in array
+    arr.append(int(input()))
+
+ele = int(input())  # for specific element
+
+index = indexEle(arr, ele)
+
+print(index+1)  # printing index of a specific element
